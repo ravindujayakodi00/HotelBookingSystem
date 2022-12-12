d20f791cfd18122110b2a3c197ccfa5ff87e2ae0
Ravindu Jayakodi <109682931+ravindujayakodi00@users.noreply.github.com>
2022-12-12 21:49:57 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,40 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const authRoute = require('./routes/auth')
+const hotelRoute = require('./routes/hotels')
+const roomRoute = require('./routes/rooms')
+const userRoute = require('./routes/users')
+
 //express app
 const app = express();
 
 //middleware
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req, res,next) => {
     console.log(req.path, req.method);
     next();
-})
+});
+
+mongoose.set('strictQuery', true);
 
 //route
-
+app.use('/api/auth',authRoute);
+app.use('/api/hotels',hotelRoute);
+app.use('/api/rooms',roomRoute);
+app.use('/api/users',userRoute);
 
 //connect to db
 
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     //listen for request
     app.listen(process.env.PORT, () => {
-      console.log("connect to db and lisning port :", process.env.PORT);
+      console.log("connect to db and listening port :", process.env.PORT);
     });
   })
   .catch((error) => {
